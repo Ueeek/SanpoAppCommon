5ae450b259c7f6bfa1147c3c7f31f690eef7f42a
Ueeek <uekikoichiro@gmail.com>
2024-12-04 21:26:34 +0900

    add package dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SanpoAppCommon",
+    platforms: [
+        .iOS(.v16),
+        .watchOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
