973ad05e84b7de70b9dda9bcd1075ccf9acf1cdb
Ueeek <uekikoichiro@gmail.com>
2024-12-04 21:44:48 +0900

    add package dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.16.1"),
-        .package(url: "https://github.com/Ueeek/composable-core-location", branch: "main")
+        .package(url: "https://github.com/Ueeek/composable-core-location", revision: "363839a87ee57bf2b73285921243dca982397eef")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
